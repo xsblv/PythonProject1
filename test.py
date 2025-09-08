30072a2bfb1c22674be039f378346de0783fd278
Ksenya Soboleva <w673rx89@yandex.ru>
2025-09-08 17:42:45 +0500

    add &!

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from selene import browser, be, have
 
 browser.open('https://www.drive.ru/')
 browser.element('[name="text"]').should(be.blank).type('8e0c7987-b7d1-4cd1-bb05-2456e97384cb').press_enter()
-browser.element('.x-secondary.x-secondary-color').should(have.text('Не нашли нужного?'))
+browser.element('.x-secondary.x-secondary-color').should(have.text('Не нашли нужного?!?'))
